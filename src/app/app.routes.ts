e59bf97a9b0c2c413bf870ded7392756fc3af039
juliaf.nery <julianery.marmota@gmail.com>
2026-07-09 01:36:46 -0300

    Modificação da página inicial

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,30 +1,17 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
-    {
-        path: "",
-        pathMatch: "full",
-        loadComponent: () => {
-            return import("./pages/inicio/inicio.component")
-                .then(c => c.InicioComponent);
-        }
-    },
-
-    {
-        path: "",
-        pathMatch: "full",
-        loadComponent: () => {
-            return import("./pages/login/login.component")
-                .then(c => c.LoginComponent);
-        }
-    },
-
-    {
-        path: "",
-        pathMatch: "full",
-        loadComponent: () => {
-            return import("./pages/home/home.component")
-                .then(c => c.HomeComponent);
-        }
-    },
+  {
+    path: '',
+    pathMatch: 'full',
+    loadComponent: () => import('./pages/inicio/inicio.component').then(c => c.InicioComponent),
+  },
+  {
+    path: 'login',
+    loadComponent: () => import('./pages/login/login.component').then(c => c.LoginComponent),
+  },
+  {
+    path: 'home',
+    loadComponent: () => import('./pages/home/home.component').then(c => c.HomeComponent),
+  },
 ];
--- src/app/pages/inicio/inicio.component.ts
+++ src/app/pages/inicio/inicio.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-inicio',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './inicio.component.html',
   styleUrl: './inicio.component.css'
 })
